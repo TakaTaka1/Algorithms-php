9162ce1834f381df7ec92bf387a062f0315082dd
root <root@localhost.localdomain>
2018-08-06 17:20:14 +0100

    Create random vocabraries for scraiping

--- scraiper.php
+++ scraiper.php
@@ -4,9 +4,15 @@ require_once './vendor/autoload.php';
 ini_set("display_errors", 1);
 error_reporting(E_ALL);
 
+
+$randomWords = ['prosperous','famine','prevail','introvert','pertain'];
+$ranNum = mt_rand(0, count($randomWords)-1);
 $client = new Goutte\Client();
+$crawler = $client->request('GET', 'https://ejje.weblio.jp/content/'.$randomWords[$ranNum]);
+//$vocab = $crawler->filter('h1')->text();
 
-$crawler = $client->request('GET', 'your URL which you want to scrape');
-$vocab = $crawler->filter('h1')->text();
+$vocab = $crawler->filter('h1')->each(function($node){
+    echo $node->text();
+});
 
-echo $vocab;
+var_dump($vocab);
