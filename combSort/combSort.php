c69a638745eda534d0a294fdf10cb8a89f101bc3
teacherAnswer69 <tk.1987.6.9@gmail.com>
2018-09-22 06:15:53 +0100

    combSort

---
+++ combSort/combSort.php
@@ -0,0 +1,36 @@
+<?php
+// Your code here!
+$arr = [10,9,3,4,1,5,68,2];
+
+// 要素の先頭と最後を比較
+// 先頭の要素をインクリメント
+// 最後の要素をデクリメントするようにロジックを考える
+
+function comSort(&$arr){
+    $gap = $count = count($arr);
+    print_r(floor($gap/1.25));
+    //$swap = true;
+    while($gap > 1){
+        if($gap > 1){
+            $gap = $gap-1;
+        }
+        $i =0;
+	while($i + $gap < $count){
+	// $i=0,
+	// $gap=5,
+	// 
+            if($arr[$i] > $arr[$i + $gap]){
+                $tmp = $arr[$i];
+                $arr[$i] = $arr[$i + $gap];
+                $arr[$i + $gap] = $tmp;
+                //$swap = true;
+            }
+            $i++;
+        }
+    }
+    return $arr;
+}
+
+print_r(comSort($arr));
+
+?>
