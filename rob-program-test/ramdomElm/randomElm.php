d1f0983619f9faa1d8b485d0f29b1b6a61ed71a7
t_hoshino <t_hoshino@robust-inc.com>
2018-09-26 15:25:41 +0900

    add or operation adn bit operation

--- rob-program-test/ramdomElm/randomElm.php
+++ rob-program-test/ramdomElm/randomElm.php
@@ -19,8 +19,50 @@ function randString($length,$opt){
     $arr = str_split($chars);
     $setStartWords =$startWords;
 
+    if(($opt & AT_LEAST_ALLTYPE | NO_DUPLICATION)){
+        print_r("AT_LEAST_ALLTYPE | NO_DUPLICATION\n");
+        $digitFlag =false;
+        $lowerFlag =false;
+        $upperFlag =false;
+        for($i=0; $i<$length-1; $i++){
+            $ranNum = mt_rand(0, count($arr)-1);
+            if(strpos($setStartWords, $arr[$ranNum]) === false){
+                $setStartWords .= $arr[$ranNum];
+            }
+            if($i === $length-2){
+                if(strlen($setStartWords) < $length){
+                    $i=-1;
+                    $setStartWords ="";
+                    $setStartWords = $startWords;
+                }
+                for($j=0; $j<strlen($setStartWords); $j++){
+                    if(ctype_digit($setStartWords[$j])){
+                        $digitFlag=true;
+                    }
+                    if(ctype_lower($setStartWords[$j])){
+                        $lowerFlag=true;
+                    }
+                    if(ctype_upper($setStartWords[$j])){
+                        $upperFlag=true;
+                    }            
+                }
+                if(!$digitFlag || !$lowerFlag || !$upperFlag){
+                    $digitFlag =false;
+                    $lowerFlag =false;
+                    $upperFlag =false;
+    
+                    $i=-1;
+                    $setStartWords = "";
+                    $setStartWords .= $startWords;
+                }
+            }
+        }            
+        return $setStartWords;
+    }
+
     // 重複無し文字列生成
     if($opt & NO_DUPLICATION){
+        print_r('NO_DUPLICATION');
         for($i=0; $i<$length-1; $i++){
             $ranNum = mt_rand(0, count($arr)-1);
             if(strpos($setStartWords, $arr[$ranNum]) === false){
@@ -39,6 +81,7 @@ function randString($length,$opt){
     
     // 大文字,小文字,数字が必ず含む文字列 
     if($opt & AT_LEAST_ALLTYPE){
+        print_r('AT_LEAST_ALLTYPE');
         $digitFlag =false;
         $lowerFlag =false;
         $upperFlag =false;
@@ -72,7 +115,9 @@ function randString($length,$opt){
     }
 }
 
-print_r(randString(10,NO_DUPLICATION)."\n");
-print_r(randString(3,AT_LEAST_ALLTYPE));
+print_r(randString(10, NO_DUPLICATION | AT_LEAST_ALLTYPE)."\n");
+
+// print_r(AT_LEAST_ALLTYPE & NO_DUPLICATION."\n");
+// print_r(AT_LEAST_ALLTYPE | NO_DUPLICATION."\n");
 
 ?>
