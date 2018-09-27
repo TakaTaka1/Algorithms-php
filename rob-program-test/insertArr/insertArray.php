0e0479b0b262c8e5d9bae83ebd41516be46efe43
TakaoHoshino <tk.1987.6.9@gmail.com>
2018-09-28 00:01:40 +0900

    delete comment

--- rob-program-test/insertArr/insertArray.php
+++ rob-program-test/insertArr/insertArray.php
@@ -1,13 +1,10 @@
 <?php
-// 配列に指定された位置に要素を挿入する関数を作成してください
-// ['A','B','C']に'D'をindex:1に挿入 => ['A','D','B','C']
-
 
 // 指定の添え字以上をtmpとして持つ
 // 連想配列が混じった配列はkeyが無いものは昇順でindexが割り振られる
 
 $arr =['A'=>'a','B','c'=>'C','D'];
-$num=4;
+$num=0;
 
 function addArrElm($inputArr=[], $num, $addWord=""){
     $count=0;
