fbc0106142f607aa1432974b0da086c2eddc4e93
TakaoHoshino <tk.1987.6.9@gmail.com>
2018-10-05 23:23:10 +0900

    add recursive

---
+++ rob-program-test/recursiveDNA/recursive.php
@@ -0,0 +1,36 @@
+<?php 
+
+// DNA
+
+function dna($n, $joinWords=""){
+   $words = ['A','C','G','T'];
+   if($n==0){
+       print_r($joinWords."\n");
+       return;
+   }
+   
+   foreach($words as $word){
+       dna($n-1, $joinWords.$word);
+   }
+}
+
+dna(3);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+?>
