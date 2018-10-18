5533cf8fd0018339806c528b32f546ac3ee2bd15
TakaoHoshino <tk.1987.6.9@gmail.com>
2018-10-18 19:05:31 +0900

    Update combSort.php

--- combSort/combSort.php
+++ combSort/combSort.php
@@ -1,5 +1,4 @@
 <?php
-// Your code here!
 $arr = [10,9,3,4,1,5,68,2];
 
 // 要素の先頭と最後を比較
@@ -9,16 +8,12 @@ $arr = [10,9,3,4,1,5,68,2];
 function comSort(&$arr){
     $gap = $count = count($arr);
     print_r(floor($gap/1.25));
-    //$swap = true;
     while($gap > 1){
         if($gap > 1){
             $gap = $gap-1;
         }
         $i =0;
-	while($i + $gap < $count){
-	// $i=0,
-	// $gap=5,
-	// 
+	while($i + $gap < $count){ 
             if($arr[$i] > $arr[$i + $gap]){
                 $tmp = $arr[$i];
                 $arr[$i] = $arr[$i + $gap];
