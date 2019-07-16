87c32d497e8e7e1a29663ee90662c7e619ab98ac
TakaTaka1 <tk.1987.6.9@gmail.com>
2019-07-16 18:22:07 +0900

    Update fib.php

--- algorithms/fib:palindrome/fib.php
+++ algorithms/fib:palindrome/fib.php
@@ -25,3 +25,52 @@ function fib($n){
 for ($i=1; $i<=10; $i++) {
     print_r(fib($i)."\n");
 }
+
+
+
+
+// palindrome
+// Palindrome
+// 以下のTODO部分を実装し、"deleveled"のように、前から読んでも、後ろから読んでも同じになる文字列の場合、true
+// そうでない場合Falseを返すような関数を実装してください。
+
+class Palindrome
+{
+    public static function isPalindrome($word)
+    {
+        // TODO
+        // 1.文字列を確認し最初の文字と最後の文字を確認する
+        // 2.最初の文字番目+nと最後の文字番目-nを比較していく
+        // 3.n番目の文字が違う場合はその文字列は、Palindromeでないと判断する
+        // 4.n番目の文字が一度も違わない場合はその文字列は、Palindromeと判断する
+        
+        // 文字列を配列に
+        $length = strlen($word);
+        // 配列の真ん中の要素を算出する
+        $middle = floor($length/2);
+        
+        // $lengthが奇数なら、$middleを使用する 偶数なら使用しない
+        
+        for($i=0; $i<$middle; $i++){
+            if($word[$i] !== $word[($length-1) - $i]){
+                return false;
+            } 
+        }
+        return true;
+    }
+}
+
+// テストケース
+// 偶数 奇数
+var_dump(Palindrome::isPalindrome('deleveled')); // true
+var_dump(Palindrome::isPalindrome('ABBA')); // true
+
+var_dump(Palindrome::isPalindrome('ABC')); // false
+var_dump(Palindrome::isPalindrome('ABCA')); // false
+
+// 文字が１文字の時
+var_dump(Palindrome::isPalindrome('d')); //true
+
+
+//var_dump(Palindrome::isPalindrome('deleveled')); // true
+//echo Palindrome::isPalindrome('deleveled'); // true
